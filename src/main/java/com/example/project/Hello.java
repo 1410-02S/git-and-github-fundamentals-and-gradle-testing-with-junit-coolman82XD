f5cafb52962af9775084064efea5d9e340797860
coolman82XD <122479566+coolman82XD@users.noreply.github.com>
2023-01-21 17:32:30 -0700

    Update Hello.java

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -3,7 +3,10 @@ package com.example.project;
 public class Hello {
 
 	public static void main(final String[] args) {
+		System.setOut(null);
 		System.out.println("Hello World!\n");
+		System.exit(1);
+		
 	}
 
 }
